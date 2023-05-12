960f20ea4e6ae4ec7731f3396d1315f4be61e017
zazul <zazul@DESKTOP-KJ2DATF>
2023-05-12 18:30:53 +0500

    Crazy driver

--- scripts/game/SCR_BestVechicleAi.c
+++ scripts/game/SCR_BestVechicleAi.c
@@ -10,7 +10,7 @@ class SCR_BestVechicleAi : ScriptComponent
 {
 	ref array<vector> positions = new array<vector>();
 	int cPosition = 0;
-	
+	int backward = 0;
 	
 	override void OnPostInit(IEntity owner)
 	{
@@ -47,23 +47,44 @@ class SCR_BestVechicleAi : ScriptComponent
 		vector origin = owner.GetOrigin();
 		vector moveVector = nextPosition - origin;
 		float length = moveVector.Length();
-		if (length > 1) moveVector.Normalize();
-		else if (length < 0.2) {
+		if (length < 4) {
 			cPosition++;
 			if (cPosition >= positions.Count())
 				cPosition = 0;
+			Print(cPosition.ToString());
 		}
-		
+		if (length > 1) moveVector.Normalize();
 		
 		
 		vector rotation = owner.GetLocalYawPitchRoll();
 		vector needRotation = moveVector.VectorToAngles();
-		float deltaAngle = SCR_Math.DeltaAngle(rotation[0] + 180, needRotation[0] + 180);
+		
+		
+		float deltaAngle = SCR_Math.fmod(rotation[0] - needRotation[0], 360);
+		if (deltaAngle >  180) deltaAngle -= 360;
+		if (deltaAngle < -180) deltaAngle += 360;
+		
+		//float deltaAngle = SCR_Math.DeltaAngle(rotation[0], needRotation[0]);
+		
+		
+		
+		
+		//if (Math.AbsFloat(rotation[0] - needRotation[0]) > 180) deltaAngle = -deltaAngle;
+		float rotateAngle = deltaAngle;
+		if (rotateAngle >  4) rotateAngle =  4;
+		if (rotateAngle < -4) rotateAngle = -4;
 		if (length > 1) {
-			physics.SetAngularVelocity(vector.Up * deltaAngle * 0.04);
+			physics.SetAngularVelocity(vector.Up * rotateAngle * -physics.GetVelocity().Length()/100);
 		}
 		vector forwardVector = rotation.AnglesToVector();
-		physics.SetVelocity(forwardVector * vector.Dot(forwardVector, moveVector) * 8);
+		
+		if (Math.AbsFloat(deltaAngle) > length*3 || backward > 0) {
+			physics.ApplyImpulse(-forwardVector * 400);
+			if (backward < 0) backward = 150;			
+		} else {
+			physics.ApplyImpulse(forwardVector * 400);
+		}
+		if (backward >= 0) backward--;
 		
 		GetGame().GetCallqueue().CallLater(sai, 0, false);
 	}
