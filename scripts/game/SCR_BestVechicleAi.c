55f502da03505caa9ef6d0e3082f3c25ec654b2d
zazul <zazul@DESKTOP-KJ2DATF>
2023-05-12 17:21:36 +0500

    Vehicle movement

--- scripts/game/SCR_BestVechicleAi.c
+++ scripts/game/SCR_BestVechicleAi.c
@@ -8,9 +8,25 @@ class SCR_BestVechicleAiClass: ScriptComponentClass
 [ComponentEditorProps(icon: HYBRID_COMPONENT_ICON)]
 class SCR_BestVechicleAi : ScriptComponent
 {
+	ref array<vector> positions = new array<vector>();
+	int cPosition = 0;
+	
+	
 	override void OnPostInit(IEntity owner)
 	{
-		sai()
+		GetGame().GetCallqueue().CallLater(saii, 1, false);
+	}
+	
+	void saii()
+	{
+		IEntity wp = GetGame().GetWorld().FindEntityByName("WP1");
+		positions.Insert(wp.GetOrigin());
+		wp = GetGame().GetWorld().FindEntityByName("WP2");
+		positions.Insert(wp.GetOrigin());
+		wp = GetGame().GetWorld().FindEntityByName("WP3");
+		positions.Insert(wp.GetOrigin());
+		
+		GetGame().GetCallqueue().CallLater(sai, 0, false);
 	}
 	
 	void sai()
@@ -19,12 +35,35 @@ class SCR_BestVechicleAi : ScriptComponent
 		Vehicle vehicle = Vehicle.Cast(owner);
 		AIPathfindingComponent aIPathfindingComponent = AIPathfindingComponent.Cast(vehicle.FindComponent(AIPathfindingComponent));
 		NavmeshWorldComponent navmeshWorldComponent = aIPathfindingComponent.GetNavmeshComponent();
-		
 		Physics physics = vehicle.GetPhysics();
+		
+		/*
 		physics.SetAngularVelocity(vector.Up * 0.5);
 		vector rotation = owner.GetLocalYawPitchRoll();
 		physics.SetVelocity(rotation.AnglesToVector() * 8);
-		//physics.ApplyTorque(vector.Right * 500000000);
+		*/
+		
+		vector nextPosition = positions[cPosition];
+		vector origin = owner.GetOrigin();
+		vector moveVector = nextPosition - origin;
+		float length = moveVector.Length();
+		if (length > 1) moveVector.Normalize();
+		else if (length < 0.2) {
+			cPosition++;
+			if (cPosition >= positions.Count())
+				cPosition = 0;
+		}
+		
+		
+		
+		vector rotation = owner.GetLocalYawPitchRoll();
+		vector needRotation = moveVector.VectorToAngles();
+		float deltaAngle = SCR_Math.DeltaAngle(rotation[0] + 180, needRotation[0] + 180);
+		if (length > 1) {
+			physics.SetAngularVelocity(vector.Up * deltaAngle * 0.04);
+		}
+		vector forwardVector = rotation.AnglesToVector();
+		physics.SetVelocity(forwardVector * vector.Dot(forwardVector, moveVector) * 8);
 		
 		GetGame().GetCallqueue().CallLater(sai, 0, false);
 	}
