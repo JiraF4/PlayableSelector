8cf4638886d2a7bda5e82ff7d1a550b8ef01abf0
Ram <ibarahnin1@gmail.com>
2023-12-21 23:43:16 +0500

    Objectives start

---
+++ scripts/game/ObjectiveSystem/PS_ObjectiveManager.c
@@ -0,0 +1,12 @@
+//------------------------------------------------------------------------------------------------
+[ComponentEditorProps(category: "GameScripted/GameMode/Components", description: "", color: "0 0 255 255", icon: HYBRID_COMPONENT_ICON)]
+class PS_ObjectiveManagerClass: ScriptComponentClass
+{
+	
+};
+
+[ComponentEditorProps(icon: HYBRID_COMPONENT_ICON)]
+class PS_ObjectiveManager : ScriptComponent
+{
+	
+};
