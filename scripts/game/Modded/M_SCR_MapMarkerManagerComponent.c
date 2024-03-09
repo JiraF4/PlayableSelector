4ca95932b9c30f8fdac496e75a0c5c9356ba8a47
Ram <ibarahnin1@gmail.com>
2024-03-10 04:14:16 +0500

    ExpFixes

--- scripts/game/Modded/M_SCR_MapMarkerManagerComponent.c
+++ scripts/game/Modded/M_SCR_MapMarkerManagerComponent.c
@@ -1,25 +1,15 @@
 // Remove squad markers
 modded class SCR_MapMarkerManagerComponent
 {
-	override protected void EOnInit(IEntity owner)
-	{	
-		if (!m_pGameMode.IsMaster())	// init server logic below
-			return;
-		
-		// If game mode coop check for flag
-		PS_GameModeCoop gameModeCoop = PS_GameModeCoop.Cast(GetGame().GetGameMode());
-		if (gameModeCoop && gameModeCoop.GetDisableLeaderSquadMarkers()) return;
-		
-		array<ref SCR_MapMarkerEntryConfig> entryCfgs = m_MarkerCfg.GetMarkerEntryConfigs();
-		foreach ( SCR_MapMarkerEntryConfig cfg : entryCfgs )
+	override void Update(float timeSlice)
+	{
+		if (!m_MapEntity)
 		{
-			SCR_MapMarkerEntryDynamic entryDynamic = SCR_MapMarkerEntryDynamic.Cast(cfg);
-			if (entryDynamic)
-				entryDynamic.InitServerLogic();
+			m_MapEntity = SCR_MapEntity.GetMapInstance();
+			m_MapEntity.GetOnMapPanEnd().Insert(OnMapPanEnd);
+			super.Update(timeSlice);
 		}
-		
-		SCR_FactionManager mgr = SCR_FactionManager.Cast(GetGame().GetFactionManager());
-		if (mgr)
-			mgr.GetOnPlayerFactionChanged_S().Insert(OnPlayerFactionChanged_S);
+		else
+			super.Update(timeSlice);
 	}
 }
