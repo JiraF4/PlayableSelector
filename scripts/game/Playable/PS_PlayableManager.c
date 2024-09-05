f8a1ed2d2cf4664136991b9eecef546298ab7e82
HubSesk <meshok.pisem29@gmail.com>
2024-09-05 19:03:16 +0500

    Revert "all non essential components deleted"
    This reverts commit 506809a24b355c862063b3846033f1b65758bcf1.

--- scripts/game/Playable/PS_PlayableManager.c
+++ scripts/game/Playable/PS_PlayableManager.c
@@ -240,7 +240,7 @@ class PS_PlayableManager : ScriptComponent
 			entity = playableController.GetInitialEntity();
 			if (!entity)
 			{
-				Resource resource = Resource.Load("{797EC030294FAAE4}Prefabs/forbidden_Initial_Player.et");
+				Resource resource = Resource.Load("{ADDE38E4119816AB}Prefabs/InitialPlayer_Version2.et");
 				EntitySpawnParams params = new EntitySpawnParams();
 				entity = GetGame().SpawnEntityPrefab(resource, GetGame().GetWorld(), params);
 				playableController.SetInitialEntity(entity);
